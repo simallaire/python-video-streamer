bb749ed9a4364cf33d5e9254c9ab25a54c93668f
Simon-Pierre Allaire <spa05@acuitysso.com>
2023-07-26 23:00:07 -0400

    Use HTTPServer instead of flask, get HTML and MJPG streams

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM spallaire93/opencv-python 
 
-RUN pip install imutils \
-    flask
+
 COPY main.py /app/main.py
 
 WORKDIR /app
--- main.py
+++ main.py
@@ -1,54 +1,86 @@
 import cv2
-import imutils
-from flask import Flask, Response
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
+import logging
 
-camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-app = Flask(__name__)
+capture = None
+clients = {}
 
-def generate():
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            # Resize the frame for faster processing
-            frame = imutils.resize(frame, width=1920)
-            # Encode the frame as JPEG
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            # Yield the frame in bytes
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-def generate_snapshot():
-    success, frame = camera.read()
-    if success:
-        # Resize the frame for faster processing
-        frame = imutils.resize(frame, width=1920)
-        # Encode the frame as JPEG
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-        # Yield the frame in bytes
-        return frame
-    else:
-        return None
+logging.basicConfig(
+    format="[%(asctime)s] %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
-    
-    
-@app.route('/stream')
-def video_feed():
-    return Response(generate(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-@app.route('/snapshot')
-def snapshot_feed():
-    snapshot = generate_snapshot()
-    if snapshot:
-        return Response(snapshot, mimetype='image/jpeg')
-    else:
-        return render_template('error.html')
+class CamHandler(BaseHTTPRequestHandler):
+        
+    def do_GET(self):
+        if self.path.endswith('.mjpg'):
+            self.send_response(200)
+            self.send_header(
+                'Content-type',
+                'multipart/x-mixed-replace; boundary=--jpgboundary'
+            )
+            self.end_headers()
+            clients[self.client_address[0]] = 0
+            while True:
+                try:
+
+                    rc, img = capture.read()
+                    if not rc:
+                        continue
+
+                    bimg = cv2.imencode('.jpg', img)[1].tobytes()
+
+                    self.send_header('Content-type', 'image/jpeg')
+                    self.send_header('Content-length', len(bimg))
+                    self.end_headers()
+
+                    self.wfile.write(bimg)
+                    self.wfile.write(b"\r\n--jpgboundary\r\n")
+                    clients[self.client_address[0]] += 1
+                    if clients[self.client_address[0]] % 1000 == 0:
+                        logger.info(f"Sent {clients[self.client_address[0]]} frames to {self.client_address[0]}")
+                        
+                except KeyboardInterrupt:
+                    self.wfile.write(b"\r\n--jpgboundary--\r\n")
+                    break
+                except BrokenPipeError:
+                    continue
+            return
+
+        if self.path.endswith('.html'):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'<html><head></head><body>')
+            self.wfile.write(b'<img src="http://192.168.0.110:8081/cam.mjpg"/>')
+            self.wfile.write(b'</body></html>')
+            return
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+
+def main():
+
+    global capture
+    capture = cv2.VideoCapture(0)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    global img
+    try:
+        server = ThreadedHTTPServer(('0.0.0.0', 8081), CamHandler)
+        logger.info("Server started at http://127.0.0.1:8081/cam.html")
+        server.serve_forever()
+    except KeyboardInterrupt:
+        capture.release()
+        server.socket.close()
 
 if __name__ == '__main__':
-    if camera.isOpened():
-        app.run(host='0.0.0.0', port=8000)
+    main()
